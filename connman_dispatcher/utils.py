3d84e8e871b1049102815136ef23e3e630461918
a-sk <a.skurihin@gmail.com>
2014-03-01 23:07:30 +0400

    Check if file is executable, before executing it

--- connman_dispatcher/utils.py
+++ connman_dispatcher/utils.py
@@ -4,6 +4,9 @@ import logbook
 logger = logbook.Logger('connman-dispatcher')
 
 
+def is_executable(path):
+    return all([os.path.isfile(path), os.access(path, os.X_OK)])
+
 def execute_scripts_in_dirs(paths, state):
     for path in sorted(paths):
         if os.path.exists(path) and os.path.isdir(path):
@@ -13,6 +16,9 @@ def execute_scripts_in_dir(path, state):
     for script in sorted(os.listdir(path)):
         full_scirpt_path = os.path.join(path, script)
         if os.path.exists(full_scirpt_path):
-            logger.info('executing: %s %s' % (full_scirpt_path, state))
-            subprocess.Popen([full_scirpt_path, state])
+            if is_executable(full_scirpt_path):
+                logger.info('executing: %s %s' % (full_scirpt_path, state))
+                subprocess.Popen([full_scirpt_path, state])
+            else:
+                logger.error('%s is not executable file' % full_scirpt_path)
 
